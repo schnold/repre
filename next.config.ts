ceede7a7e881e9edd70cdfd4b5c502d3e538e24b
schnold <lukas.knaebel@protonmail.com>
2025-01-07 14:33:23 +0100

    1.0

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,46 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://repre.io',
+        permanent: true,
+        has: [
+          {
+            type: 'host',
+            value: 'app.repre.io',
+          },
+        ],
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
